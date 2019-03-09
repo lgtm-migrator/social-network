cbce7eeb7e6a819fe4644a4b1feef2b4ec0eb0c5
anyulled <anyulled@gmail.com>
2019-03-09 04:08:56 +0100

    adding more params

--- src/main/java/com/schibsted/spain/friends/utils/Utils.java
+++ src/main/java/com/schibsted/spain/friends/utils/Utils.java
@@ -1,13 +1,24 @@
 package com.schibsted.spain.friends.utils;
 
-public class Utils {
+/**
+ * Utility class for request mappings and params
+ */
+public final class Utils {
 
     public static final String FRIENDSHIP_LIST_URL = "/friendship/list";
     public static final String FRIENDSHIP_DECLINE_URL = "/friendship/decline";
     public static final String FRIENDSHIP_ACCEPT_URL = "friendship/accept";
     public static final String FRIENDSHIP_REQUEST_URL = "/friendship/request";
+    public static final String USERNAME_FROM = "usernameFrom";
+    public static final String USERNAME_TO = "usernameTo";
+    public static final String X_PASS = "X-Password";
+    public static final String USERNAME = "username";
+    public static final String SIGN_UP_URL = "/signup";
 
+    /**
+     * utility classes should not be instantiated
+     */
     private Utils() {
-
+        //NOSONAR
     }
 }
