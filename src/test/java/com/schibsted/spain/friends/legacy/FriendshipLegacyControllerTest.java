c43ecad0352a241315711a43c9ba035a56f969f8
alrs <anyul.rivas@gft.com>
2019-03-19 12:16:58 +0100

    SONAR fixes

--- src/test/java/com/schibsted/spain/friends/legacy/FriendshipLegacyControllerTest.java
+++ src/test/java/com/schibsted/spain/friends/legacy/FriendshipLegacyControllerTest.java
@@ -47,9 +47,9 @@ class FriendshipLegacyControllerTest {
     private static final String ROSS_PASS = "r3456789";
     private static final String PETER_PASS = "p4567890";
     private static final String ROB_PASS = "r0123456";
-    private static final String A_PENDING_FRIEND_REQUEST_ALREADY_EXISTS = "A pending friend request already exists";
-    private static final String SAMANTHA_DOESN_T_EXISTS = "Samantha doesn't exists";
-    private static final String INVALID_CREDENTIALS = "Invalid credentials for";
+    private static final String DOESNT_EXISTS = " doesn't exists";
+    private static final String INVALID_CREDENTIALS = "Invalid credentials for ";
+    private static final String A_PENDING_FRIEND_REQUEST_ALREADY_EXISTS1 = "A pending friend request already exists";
 
     @Autowired
     UserService userService;
@@ -74,7 +74,7 @@ class FriendshipLegacyControllerTest {
         when(userRepository.findUser(JOHN_DOE_STR, JOHN_PASS)).thenReturn(User.builder().username(JOHN_DOE_STR).password(JOHN_PASS).build());
         when(userRepository.findUser(JOHN_DOE_STR, "j12345679")).thenThrow(new InvalidCredentialException(INVALID_CREDENTIALS + JOHN_DOE_STR));
 
-        when(userRepository.findUser(SAMANTHA_STR, "x12345678")).thenThrow(new InvalidCredentialException(SAMANTHA_DOESN_T_EXISTS));
+        when(userRepository.findUser(SAMANTHA_STR, "x12345678")).thenThrow(new InvalidCredentialException(SAMANTHA_STR + DOESNT_EXISTS));
 
         when(userRepository.findUser(ROSEANNE_STR, ROSS_PASS)).thenReturn(roseanne);
         when(userRepository.findUser(ROSEANNE_STR, "r3456780")).thenThrow(new InvalidCredentialException(INVALID_CREDENTIALS + ROSEANNE_STR));
@@ -108,7 +108,7 @@ class FriendshipLegacyControllerTest {
     @Test
     @DisplayName("should be able to do a friend request only once")
     void friendRequestTestCase() throws Exception {
-        when(userRepository.getUser(SAMANTHA_STR)).thenThrow(new NotFoundException("Samantha doesn't exists"));
+        when(userRepository.getUser(SAMANTHA_STR)).thenThrow(new NotFoundException(SAMANTHA_STR + DOESNT_EXISTS));
         when(friendshipRepository.requestFriendship(johnDoe, roseanne))
                 .thenReturn(FriendshipRequest.builder()
                         .userFrom(johnDoe)
@@ -226,7 +226,7 @@ class FriendshipLegacyControllerTest {
                         .userTo(johnDoe)
                         .status(ACCEPTED)
                         .build())
-                .thenThrow(new AlreadyExistsException(A_PENDING_FRIEND_REQUEST_ALREADY_EXISTS));
+                .thenThrow(new AlreadyExistsException(A_PENDING_FRIEND_REQUEST_ALREADY_EXISTS1));
 
         when(userRepository.addFriend(roseanne, johnDoe)).thenReturn(roseanne);
 
@@ -265,10 +265,10 @@ class FriendshipLegacyControllerTest {
                         .userTo(johnDoe)
                         .status(DECLINED)
                         .build())
-                .thenThrow(new AlreadyExistsException("A pending friend request already exists"));
+                .thenThrow(new AlreadyExistsException(A_PENDING_FRIEND_REQUEST_ALREADY_EXISTS1));
 
         when(friendshipRepository.declineFriendship(peter, jessica))
-                .thenThrow(new NotFoundException("A pending friend request already exists"));
+                .thenThrow(new NotFoundException(A_PENDING_FRIEND_REQUEST_ALREADY_EXISTS1));
 
         mockMvc.perform(post(FRIENDSHIP_MAPPING + DECLINE)
                 .param(USERNAME_FROM, PETER_STR)
@@ -366,7 +366,7 @@ class FriendshipLegacyControllerTest {
     @Test
     @DisplayName("should be able to list friends of signed in users")
     void listFriends() throws Exception {
-        when(userRepository.getUser(SAMANTHA_STR)).thenThrow(new NotFoundException("Samantha doesn't exists"));
+        when(userRepository.getUser(SAMANTHA_STR)).thenThrow(new NotFoundException(SAMANTHA_STR + DOESNT_EXISTS));
         when(userRepository.getUser(JOHN_DOE_STR))
                 .thenReturn(User.builder().username(JOHN_DOE_STR)
                         .friend(roseanne)
