45716987fe1afc1b2d564efb865efcaa1464002e
anyulled <anyulled@gmail.com>
2019-03-24 20:19:44 +0100

    feat: improving logging aspect and logback log

--- src/main/java/com/schibsted/spain/friends/config/LoggingAspect.java
+++ src/main/java/com/schibsted/spain/friends/config/LoggingAspect.java
@@ -2,9 +2,11 @@ package com.schibsted.spain.friends.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -14,33 +16,54 @@ import java.util.Arrays;
 @Component
 public class LoggingAspect {
 
-    @Before("execution(* com.schibsted.spain.friends.legacy.*.*(..))")
-    public void logBeforeAllControllerMethods(JoinPoint joinPoint) {
-        log.info("== Legacy Controller invoked : {} ==", joinPoint.getSignature().getName());
+    /**
+     * Pointcut that matches all repositories, services and Web REST endpoints.
+     */
+    @Pointcut("within(@org.springframework.stereotype.Repository *)" +
+            " || within(@org.springframework.stereotype.Service *)" +
+            " || within(@org.springframework.web.bind.annotation.RestController *)")
+    public void springBeanPointcut() {
+        // Method is empty as this is just a Pointcut, the implementations are in the advices.
     }
 
-    @After("execution(* com.schibsted.spain.friends.legacy.*.*(..))")
-    public void logAfterAllControllerMethods(JoinPoint joinPoint) {
-        log.info("== Legacy Controller finished : {} ==", joinPoint.getSignature().getName());
+    /**
+     * Pointcut that matches all Spring beans in the application's main packages.
+     */
+    @Pointcut("within(com.schibsted.spain.friends.repository..*) || within(com.schibsted.spain.friends.service..*) || within(com.schibsted.spain.friends.legacy..*)")
+    public void applicationPackagePointcut() {
+        // Method is empty as this is just a Pointcut, the implementations are in the advices.
     }
 
-    @Before("execution(* com.schibsted.spain.friends.service.*.*(..))")
-    public void logBeforeAllServiceMethods(JoinPoint joinPoint) {
-        log.info("== Service method {} invoked with params {} ==", joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
+    @Around("springBeanPointcut()")
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        if (log.isDebugEnabled()) {
+            log.debug("Entering: {}.{}() with argument[s] = {}", joinPoint.getSignature().getDeclaringTypeName(),
+                    joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
+        }
+        try {
+            Object result = joinPoint.proceed();
+            if (log.isDebugEnabled()) {
+                log.debug("Finished: {}.{}() with result = {}", joinPoint.getSignature().getDeclaringTypeName(),
+                        joinPoint.getSignature().getName(), result);
+            }
+            return result;
+        } catch (IllegalArgumentException e) {
+            log.error("Illegal argument: {} in {}.{}()", Arrays.toString(joinPoint.getArgs()),
+                    joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
+
+            throw e;
+        }
     }
 
-    @After("execution(* com.schibsted.spain.friends.service.*.*(..))")
-    public void logAfterAllServiceMethods(JoinPoint joinPoint) {
-        log.info("==  Service finished : {} ==", joinPoint.getSignature().getName());
-    }
+    @AfterThrowing(pointcut = "springBeanPointcut() ", throwing = "e")
+    public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
+        if (log.isDebugEnabled()) {
+            log.error("Exception in {}.{}() with cause = \'{}\' and exception = \'{}\'", joinPoint.getSignature().getDeclaringTypeName(),
+                    joinPoint.getSignature().getName(), e.getCause() != null ? e.getCause() : "NULL", e.getMessage(), e);
 
-    @Before("execution(* com.schibsted.spain.friends.repository.*.*(..))")
-    public void logBeforeAllRepositoryMethods(JoinPoint joinPoint) {
-        log.info("== Repository method {} invoked with params {}==", joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
-    }
-
-    @After("execution(* com.schibsted.spain.friends.repository.*.*(..))")
-    public void logAfterAllRepositoryMethods(JoinPoint joinPoint) {
-        log.info("== Repository finished : {} ==", joinPoint.getSignature().getName());
+        } else {
+            log.error("Exception in {}.{}() with cause = {}", joinPoint.getSignature().getDeclaringTypeName(),
+                    joinPoint.getSignature().getName(), e.getCause() != null ? e.getCause() : "NULL");
+        }
     }
 }
